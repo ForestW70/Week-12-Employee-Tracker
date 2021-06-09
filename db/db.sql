61dd6b267991526b7e28faf90fa8c9e76f23bdc3
ForestW70 <hexaforest@gmail.com>
2021-06-09 11:55:56 -0400

    updated sql file to include seeds. started prompt scripts.

--- db/db.sql
+++ db/db.sql
@@ -3,25 +3,61 @@ CREATE DATABASE hw12_db;
 USE hw12_db;
 
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(9,2),
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
+
+
+INSERT INTO departments(name)
+VALUES 
+("Manager"),
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal"),
+("HR");
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+("Manager", 900000, 1),
+("Sales person", 34567.89, 2),
+("Full Stack Developer", 120000, 3),
+("Accountant", 100000, 4),
+("Lawyer", 50.99, 5),
+("HR", 70000, 6);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES 
+("Forest", "Wilson", 1, 1), 
+("Worest", "Filson", 2, NULL), 
+("S'morest", "S'milson", 3, NULL),
+("Barack", "Obama", 4, 2),
+("Kony", "Twentytwelve", 5, NULL),
+("Karen", "McKarensson", 6, 3),
+("Bebe", "Johnstone", 1, NULL),
+("Helga", "Zimboski", 2, NULL),
+("Montgomry", "McGee", 3, NULL),
+("Simple", "John", 4, NULL),
+("Alyssa", "Kurke", 5, 4);
--- index.js
+++ index.js
@@ -5,9 +5,57 @@ const initialize = () => {
     inquirer
         .prompt([
             {
-                type: 'input',
-                name: 'name',
-                message: 'What is your name?',
+                type: 'list',
+                name: 'path',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all employees',
+                    'View all employees by department',
+                    'View all employees by manager',
+                    'Add employees',
+                    'Remove employee',
+                    'Update employee role',
+                    'Update employee manager'
+                ],
+            },
+        ]).then(data => {
+            switch (data.path) {
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'View all employees by department':
+                    viewEmployeesByDept();
+                    break;
+                case 'View all employees by manager':
+                    viewEmployeesByMgmt();
+                    break;
+                case 'Add employees':
+                    addEmployees();
+                    break;
+                case 'Remove employee':
+                    removeEmployee();
+                    break;
+                case 'Update employee role':
+                    updateEmployee();
+                    break;
+                case 'Update employee manager':
+                    updateEmployeeMgmt();
+                    break;
+                default:
+                    console.log("Error, no selection!");
+            }
+        })
+}
+
+const viewEmployees = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: '',
+                message: 'What would you like to do?',
             }
         ])
 }
+
+initialize();
