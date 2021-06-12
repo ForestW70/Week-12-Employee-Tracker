171ed71ff6c2c27b384fcf87dc3e5be1c4315e99
ForestW70 <hexaforest@gmail.com>
2021-06-12 17:20:10 -0400

    added remove employee function and exit program function. played around with creating a class but idk

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const mysql = require("mysql");
 const consoleTable = require("console.table");
-
-
+const { Table } = require('console-table-printer');
+const GuiTable = require("./lib/tableMaker.js");
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -10,15 +10,20 @@ const connection = mysql.createConnection({
     password: '',
     database: 'hw12_db',
 });
-
 connection.connect(err => {
-    if (err) throw err;
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
     console.log(`connected as id ${connection.threadId}\n`);
     initialize();
 });
 
-const initialize = () => {
-    inquirer
+
+
+const initialize = async () => {
+
+    await inquirer
         .prompt([
             {
                 type: 'list',
@@ -34,10 +39,12 @@ const initialize = () => {
                     'Update employee manager',
                     'Update departments',
                     'View budget',
-                    'exit'
+                    'Exit'
                 ],
             },
         ]).then(data => {
+
+            refreshEmployeeList()
             switch (data.path) {
                 case 'View all employees':
                     viewEmployees('all');
@@ -58,12 +65,50 @@ const initialize = () => {
                     updateEmployeeRole();
                     break;
                 case 'Update employee manager':
-                    updateEmployees('mgmt');
+                    updateEmployeeManager();
+                    break;
+                case 'Update departments':
+                    updateDepartments();
+                    break;
+                case 'View budget':
+                    viewBudget();
+                    break;
+                case 'Exit':
+                    promptExit();
                     break;
                 default:
                     console.log('Error, no selection!');
             }
         })
+}
+
+
+let allEmployees = [];
+
+function refreshEmployeeList() {
+    connection.query('SELECT * FROM employees;', async(err, res) => {
+        if (err) {
+            throw err.code;
+        }
+        let employees = await res
+        allEmployees = [];
+        employees.forEach(emp => {
+            let empInfo = {
+                "first_name": emp.first_name,
+                "last_name": emp.last_name,
+                "emp_id": emp.id,
+                "has_boss": emp.has_boss,
+                "role_id": emp.role_id,
+                "manager_id": emp.manager_id
+
+            };
+            allEmployees.push(empInfo);
+        })
+        
+        // console.table(allEmployees);
+
+
+    })
 }
 
 const viewEmployees = async (action) => {
@@ -73,6 +118,8 @@ const viewEmployees = async (action) => {
 
     if (action == 'all') {
         query = 'SELECT * FROM employees';
+
+        // console.table(allEmployees);
 
 
     } else if (action == 'dept') {
@@ -123,14 +170,16 @@ const viewEmployees = async (action) => {
 
     connection.query(query, (err, res) => {
         if (err) throw err;
-        console.log(res)
+        console.log("\n" + "~*employees~*" + "\n")
+        console.table(res)
+        console.log("\n");
     })
     initialize();
 }
 
 
 
-const addEmployee = async() => {
+const addEmployee = async () => {
     await inquirer.prompt([
         {
             type: 'input',
@@ -195,53 +244,70 @@ const addEmployee = async() => {
                 console.log(`Sucessfully added ${data.first_name} to the Team!`)
                 initialize();
             })
-        })
+    })
 }
 
-const removeEmployee = async() => {
-    await inquirer.prompt([
-        {
-            type: 'input',
-            name: "first_name",
-            message: "Please enter the new employee's first name:",
-        },
-        {
-            type: 'input',
-            name: "last_name",
-            message: "Please enter the new employee's last name:",
-        },
-        {
-            type: 'list',
-            name: "role_id",
-            message: "Please enter the new employee's department:",
-            choices: [
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6'
-            ]
-        },
-        {
-            type: 'confirm',
-            name: "isManager",
-            message: "Is this new employee a manager?",
-
-        },
-        {
-            type: 'list',
-            name: "has_boss",
-            message: "Who is the new employee's boss?",
-            choices: [
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6'
-            ]
+const removeEmployee = () => {
+    let allNames = [];
+    connection.query('select * from employees;', async (err, res) => {
+        if (err) {
+            throw new Error(err)
         }
-    ])
+        res.forEach(person => {
+            allNames.push(person.first_name + ' ' + person.last_name)
+        })
+
+        await inquirer.prompt([
+            {
+                type: 'list',
+                name: "emp_name",
+                message: "Please select the employee you would like to remove:",
+                choices: allNames
+            },
+
+        ]).then(data => {
+            let selcEmpFirst = data.emp_name.split(' ')[0];
+            let selcEmpLast = data.emp_name.split(' ')[1];
+
+            inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: 'isSure',
+                    message: `Are you sure you would like to delete ${data.emp_name}?`
+                }
+            ]).then(data => {
+                if (data.isSure) {
+                    let query = 'DELETE FROM employees WHERE first_name = ? AND last_name = ?;'
+                    connection.query(query, [selcEmpFirst, selcEmpLast], (err, res) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log(`${selcEmpFirst} has been removed from the company!`);
+                        initialize();
+                    })
+                } else {
+                    console.log("\n" + "Returning...")
+                    initialize();
+                }
+            })
+        })
+    })
+}
+const promptExit = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: "isDone",
+                message: "Are you sure you would like to quit?"
+            }
+        ]).then(data => {
+            if (!data.isDone) {
+                initialize();
+            } else {
+                console.log("\n" + "Come back soon!")
+                connection.end();
+            }
+        })
 }
 
